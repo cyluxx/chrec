6a15d88cc5ccc240ae193683ee53d7844178ad5b
aplewnia <andreas.plewnia@materna.de>
2018-07-19 16:56:14 +0200

    webviewTag fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,53 @@
-import 'zone.js/dist/zone-mix';
-import 'reflect-metadata';
-import '../polyfills';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import "zone.js/dist/zone-mix";
+import "reflect-metadata";
+import "../polyfills";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
 // NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 // Bootstrap
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { ElectronService } from './providers/electron.service';
+import { ElectronService } from "./providers/electron.service";
 
-import { WebviewDirective } from './directives/webview.directive';
+import { WebviewDirective } from "./directives/webview.directive";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { BrowserwindowComponent } from './components/home/components/browserwindow/browserwindow.component';
-import { QuickbarComponent } from './components/home/components/quickbar/quickbar.component';
-import { SidebarComponent } from './components/home/components/sidebar/sidebar.component';
-import { StatusbarComponent } from './components/home/components/statusbar/statusbar.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BrowserwindowComponent } from "./components/home/components/browserwindow/browserwindow.component";
+import { QuickbarComponent } from "./components/home/components/quickbar/quickbar.component";
+import { SidebarComponent } from "./components/home/components/sidebar/sidebar.component";
+import { StatusbarComponent } from "./components/home/components/statusbar/statusbar.component";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 //Font Awesome
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlay, faPause, faCircle, faSquare, faArrowLeft, faArrowRight, faArrowAltCircleRight, faFastForward, faFastBackward, faStepForward, faStepBackward, faSync } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faPlay,
+  faPause,
+  faCircle,
+  faSquare,
+  faArrowLeft,
+  faArrowRight,
+  faFastForward,
+  faFastBackward,
+  faStepForward,
+  faStepBackward,
+  faSync
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faPlay);
 library.add(faPause);
@@ -68,7 +80,7 @@ library.add(faStepBackward);
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (HttpLoaderFactory),
+        useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
     }),
@@ -78,4 +90,4 @@ library.add(faStepBackward);
   providers: [ElectronService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/components/browserwindow/browserwindow.component.ts
+++ src/app/components/home/components/browserwindow/browserwindow.component.ts
@@ -1,61 +1,64 @@
 import { Component, ViewChild, AfterViewInit } from "@angular/core";
+import { WebviewTag } from "electron";
 
 @Component({
-    selector: 'browserwindow',
-    templateUrl: './browserwindow.component.html',
-    styleUrls: ['./browserwindow.component.scss']
+  selector: "browserwindow",
+  templateUrl: "./browserwindow.component.html",
+  styleUrls: ["./browserwindow.component.scss"]
 })
 export class BrowserwindowComponent implements AfterViewInit {
-    @ViewChild('webview') webview: any;
+  @ViewChild("webview") webview: any;
 
-    inputUrl: String;
-    webviewUrl: String;
+  inputUrl: String;
+  webviewUrl: String;
 
-    constructor() {
-        this.webviewUrl = this.inputUrl = 'https://www.google.com';
+  constructor() {
+    this.webviewUrl = this.inputUrl = "https://www.google.com";
+  }
+
+  ngAfterViewInit(): void {
+    this.webview = this.webview.nativeElement;
+    this.webview as WebviewTag;
+  }
+
+  canGoBack(): boolean {
+    return this.webview.canGoBack();
+  }
+
+  onBack(): void {
+    if (this.canGoBack) {
+      this.webview.goBack();
+      this.webview.addEventListener("dom-ready", () => {
+        this.inputUrl = this.webview.getURL();
+      });
     }
+  }
 
-    ngAfterViewInit(): void {
-        this.webview = this.webview.nativeElement;   
-    }
+  canGoForward(): boolean {
+    return this.webview.canGoForward();
+  }
 
-    canGoBack(): boolean {
-        return this.webview.canGoBack();
+  onForward(): void {
+    if (this.canGoForward) {
+      this.webview.goForward();
+      this.webview.addEventListener("dom-ready", () => {
+        this.inputUrl = this.webview.getURL();
+      });
     }
+  }
 
-    onBack(): void {
-        if (this.canGoBack) {
-            this.webview.goBack();
-            while (this.webview.isLoading()) { }
-            return this.inputUrl = this.webview.getURL();
-        }
-    }
+  onReload(): void {
+    this.webview.reload();
+    this.inputUrl = this.webviewUrl;
+  }
 
-    canGoForward(): boolean {
-        return this.webview.canGoForward();
+  onUpdateWebviewUrl(): void {
+    let https: String = this.inputUrl.slice(0, 8).toLowerCase();
+    let http: String = this.inputUrl.slice(0, 7).toLowerCase();
+    if (https === "https://" || http === "http://") {
+      this.webviewUrl = this.inputUrl;
+    } else {
+      this.webviewUrl = this.inputUrl = "https://" + this.inputUrl;
     }
-
-    onForward(): void {
-        if (this.canGoForward) {
-            this.webview.goForward();
-            while (this.webview.isLoading()) { }
-            return this.inputUrl = this.webview.getURL();
-        }
-    }
-
-    onReload(): void {
-        this.webview.reload();
-        this.inputUrl = this.webviewUrl;
-    }
-
-    onUpdateWebviewUrl(): void {
-        let https: String = this.inputUrl.slice(0, 8).toLowerCase();
-        let http: String = this.inputUrl.slice(0, 7).toLowerCase();
-        if (https === 'https://' || http === 'http://') {
-            this.webviewUrl = this.inputUrl;
-        }
-        else {
-            this.webviewUrl = this.inputUrl = 'https://' + this.inputUrl;
-        }
-    }
+  }
 }
