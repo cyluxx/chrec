2d0f9323fe2d7bd99d7c5c1a95d67ea09b46f526
aplewnia <andreas.plewnia@materna.de>
2019-03-21 17:34:28 +0100

    Refactored Look and Feel

--- src/app/components/home/sequence/sequence.component.ts
+++ src/app/components/home/sequence/sequence.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ReplayService } from '../../../providers/replay.service';
+import { Sequence } from 'chrec-core/lib/model/sequence';
+import { Settings } from '../../../model/settings';
 
 @Component({
   selector: 'app-sequence',
   templateUrl: './sequence.component.html',
   styleUrls: ['./sequence.component.scss']
 })
-export class SequenceComponent implements OnInit {
+export class SequenceComponent{
 
-  constructor() { }
+  moreTestResults = false;
+  @Input() sequence: Sequence;
+  @Input() settings: Settings;
 
-  ngOnInit() {
+  constructor(private replayService: ReplayService) { }
+
+  onReRecordSequence() {
+    
   }
 
+  async onTestSequence() {
+    this.sequence = await this.replayService.testSequence(this.sequence, this.settings);
+  }
+
+  onToggleMoreTestResults() {
+    this.moreTestResults = !this.moreTestResults;
+  }
 }
