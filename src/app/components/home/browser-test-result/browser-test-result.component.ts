c524b17b0290015ea15d877148d4f04bf99b9ebc
Andreas Plewnia <acplewnia@gmail.com>
2019-03-24 19:11:27 +0100

    Refactoring

--- src/app/components/home/browser-test-result/browser-test-result.component.ts
+++ src/app/components/home/browser-test-result/browser-test-result.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BrowserTestResult } from 'chrec-core/lib/model/test-result/browser-test-result';
 import { ActionTestResult } from 'chrec-core/lib/model/test-result/action-test-result';
+import { Browser } from 'chrec-core/lib/model/browser/browser';
+import { Chrome } from 'chrec-core/lib/model/browser/chrome';
+import { Edge } from 'chrec-core/lib/model/browser/edge';
+import { Firefox } from 'chrec-core/lib/model/browser/firefox';
 
 @Component({
   selector: 'app-browser-test-result',
@@ -15,4 +19,15 @@ export class BrowserTestResultComponent {
 
   constructor() { }
 
+  getIcon(browser: Browser): string[] {
+    if (browser instanceof Chrome) {
+      return ['fab', 'chrome'];
+    }
+    if (browser instanceof Edge) {
+      return ['fab', 'edge'];
+    }
+    if (browser instanceof Firefox) {
+      return ['fab', 'firefox'];
+    }
+  }
 }
