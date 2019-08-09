743534f6806cf2f640552c7a74ced32cf10777c0
aplewnia <andreas.plewnia@materna.de>
2019-08-09 18:14:07 +0200

    #10 Reworked Preload Script

--- src/assets/preload-scripts/preload.js
+++ src/assets/preload-scripts/preload.js
@@ -6,10 +6,6 @@ const optimalSelect = require('optimal-select').select;
 const selectorQuery = require('selector-query');
 const RobulaPlus = require('./robula-plus/robula-plus').RobulaPlus;
 
-const clickableElements = 'a, button, input[type="checkbox"]';
-const readableElements = 'div, h1, h2, h3, h4, h5, h6, label, li, p, span';
-const typeableElements = 'input, textarea';
-
 global.myapi = {
   CssSelectorGenerator: CssSelectorGenerator,
   finder: finder,
@@ -20,250 +16,10 @@ global.myapi = {
 }
 const cssSelectorGenerator = new myapi.CssSelectorGenerator;
 const robulaPlus = new myapi.RobulaPlus;
+const iframes = [];
 
-const mutationObserver = new MutationObserver(() => {
-  window.eventRecorder.removeEventListeners();
-  window.eventRecorder = new EventRecorder();
-});
-
-let eventTargetValueString = '';
-let previousEventTargetValueString = '';
-let currentSelection = '';
-
-let currentElement = null;
-
-const sendClick = (event) => {
-  event = event || window.event;
-  let target = event.target || event.srcElement;
-  if (!currentElement) {
-    event.preventDefault();
-    target.style.outline = '#f00 solid 2px';
-    const message = {
-      className: 'Click',
-      locators: generateLocators(event),
-      boundingBox: target.getBoundingClientRect(),
-      target: target
-    }
-    sendAction(message);
-    currentElement = target;
-  } else {
-    currentElement.style.outline = null;
-    currentElement = null;
-  }
-};
-
-const sendMouseup = (event) => {
-  event = event || window.event;
-  let target = event.target || event.srcElement;
-  event.stopPropagation();
-  const selection = window.getSelection().toString();
-  if (selection && selection !== currentSelection) {
-    let message = {
-      className: 'Read',
-      locators: generateLocators(event),
-      boundingBox: target.getBoundingClientRect(),
-      value: selection
-    }
-    sendAction(message);
-    currentSelection = selection;
-  }
-};
-
-const sendKeyup = (event) => {
-  event = event || window.event;
-  let target = event.target || event.srcElement;
-  if (event.key === 'Enter') {
-    const message = {
-      className: 'Type',
-      locators: generateLocators(event),
-      boundingBox: target.getBoundingClientRect(),
-      value: eventTargetValueString,
-      key: '\\ue007'
-    };
-    if (previousEventTargetValueString !== eventTargetValueString) {
-      sendAction(message);
-    }
-  } else if (event.key === 'Tab') {
-    const message = {
-      className: 'Type',
-      locators: generateLocators(event),
-      boundingBox: target.getBoundingClientRect(),
-      value: eventTargetValueString,
-      key: '\\ue004'
-    };
-    if (previousEventTargetValueString !== eventTargetValueString) {
-      sendAction(message);
-    }
-  } else {
-    eventTargetValueString = target.value;
-  }
-}
-
-const sendBlur = (event) => {
-  let target = event.target || event.srcElement;
-  let message = {
-    className: 'Type',
-    locators: generateLocators(event),
-    boundingBox: target.getBoundingClientRect(),
-    value: eventTargetValueString,
-    key: ''
-  }
-  if (previousEventTargetValueString !== eventTargetValueString) {
-    sendAction(message);
-  }
-}
-
-function sendAction(message) {
-  console.log('Preload: Sending ' + message.className + ' message...');
-  ipcRenderer.sendToHost(JSON.stringify(message));
-  previousEventTargetValueString = eventTargetValueString;
-}
-
-function generateLocators(event) {
-  let locators = [];
-  let locator;
-
-  locator = cssSelectorGenerator.getSelector(event.target);
-  locators.push({ methodName: 'CssSelectorGenerator', className: 'CssLocator', value: locator });
-
-  locator = myapi.finder(event.target);
-  locators.push({ methodName: 'Finder', className: 'CssLocator', value: locator });
-
-  locator = myapi.getQuerySelector(event.target);
-  locators.push({ methodName: 'GetQuerySelector', className: 'CssLocator', value: locator });
-
-  locator = myapi.select(event.target)
-  locators.push({ methodName: 'OptimalSelect', className: 'CssLocator', value: locator });
-
-  locator = myapi.selectorQuery(event.target);
-  locators.push({ methodName: 'SelectorQuery', className: 'CssLocator', value: locator });
-
-  locator = robulaPlus.getRobustXPath(event.target, document);
-  locators.push({ methodName: 'RobulaPlus', className: 'XpathLocator', value: locator });
-
-  return locators;
-}
-
-class EventRecorder {
-  constructor() {
-    this.clickableElements = document.querySelectorAll(clickableElements);
-    this.readableElements = document.querySelectorAll(readableElements);
-    this.typeableElements = document.querySelectorAll(typeableElements);
-
-    this.addEventListeners();
-  }
-
-  addEventListeners() {
-    for (let element of this.clickableElements) {
-      element.addEventListener('click', sendClick);
-    }
-    for (let element of this.readableElements) {
-      element.addEventListener('mouseup', sendMouseup);
-    }
-    for (let element of this.typeableElements) {
-      element.addEventListener('keyup', sendKeyup);
-      element.addEventListener('blur', sendBlur);
-    }
-  }
-
-  removeEventListeners() {
-    for (let element of this.clickableElements) {
-      element.removeEventListener('click', sendClick);
-    }
-    for (let element of this.readableElements) {
-      element.addEventListener('mouseup', sendMouseup);
-    }
-    for (let element of this.typeableElements) {
-      element.removeEventListener('keyup', sendKeyup);
-      element.removeEventListener('blur', sendBlur);
-    }
-  }
-}
-
-// function addMutationObserver(target) {
-//   const mo = new MutationObserver((records, instance) => {
-//     console.log(records);
-//     records.forEach((record) => {
-//       record.addedNodes.forEach((node) => {
-//         if (node.nodeName !== '#text') {
-//           node.querySelectorAll(clickableElements).forEach((element) => {
-//             element.addEventListener('click', sendClick);
-//           });
-//           node.querySelectorAll(readableElements).forEach((element) => {
-//             element.addEventListener('mouseup', sendMouseup);
-//           });
-//           node.querySelectorAll(typeableElements).forEach((element) => {
-//             element.addEventListener('keyup', sendKeyup);
-//             element.addEventListener('blur', sendBlur);
-//           });
-//         }
-//       });
-//     });
-//   });
-//   const options = {
-//     childList: true,
-//     subtree: true,
-//     attributes: true,
-//     characterData: true,
-//     attributeOldValue: true,
-//     characterDataOldValue: true
-//   };
-//   mo.observe(target, options);
-// }
-
-// function initIframe(iframe) {
-//   if (iframes.indexOf(iframe) > -1) {
-//     return;
-//   }
-
-//   iframes.push(iframe);
-//   addMutationObserver(iframe.contentDocument.body);
-
-//   iframe.addEventListener('mouseenter', function (e) {
-//     stack.unshift(this);
-//     iframe.contentDocument.querySelectorAll('iframe').forEach(initIframe);
-//   });
-
-//   iframe.addEventListener('mouseleave', function (e) {
-//     console.log(`switch to ${stack.length > 1 ? 'parent' : 'default content'}`);
-
-//     stack.shift();
-//   });
-
-//   waitUntilIframeIsLoaded(iframe, () => {
-//     const doc = iframe.contentDocument;
-//     doc.querySelectorAll(clickableElements).forEach((element) => {
-//       element.addEventListener('click', sendClick);
-//     });
-//     doc.querySelectorAll(readableElements).forEach((element) => {
-//       element.addEventListener('mouseup', sendMouseup);
-//     });
-//     doc.querySelectorAll(typeableElements).forEach((element) => {
-//       element.addEventListener('keyup', sendKeyup);
-//       element.addEventListener('blur', sendBlur);
-//     });
-//   });
-// }
-
-// function trackMouseMovements(el) {
-//   el.addEventListener('mousemove', function (e) {
-//     if (currentTarget !== e.target) {
-//       currentTarget = e.target;
-//     }
-//   });
-// }
-
-// function waitUntilIframeIsLoaded(iframe, fn) {
-//   const win = iframe.contentWindow;
-//   if (win && win.document && win.document.body && win.document.body.innerHTML) {
-//     fn();
-//     return;
-//   }
-
-//   window.setTimeout(() => {
-//     waitUntilIframeIsLoaded(iframe, fn);
-//   }, 100);
-// }
+let stack = [];
+let currentTarget = null;
 
 // wait for document ready (without jQuery)
 if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
@@ -275,19 +31,238 @@ if (document.attachEvent ? document.readyState === "complete" : document.readySt
 }
 
 function ready() {
-  window.eventRecorder = new EventRecorder();
-  mutationObserver.observe(document, { attributes: true, childList: true, subtree: true });
-  // document.body.querySelectorAll('iframe').forEach(initIframe);
-  // trackMouseMovements(document.body);
+  document.body.querySelectorAll('iframe').forEach(initIframe);
 
-  ipcRenderer.on('pageCaptured', (event, args) => {
-    switch (args.className) {
-      case 'Click':
-        currentElement.click();
-        break;
+  addMutationObserver(document.body);
+  trackMouseMovements(document.body);
+  trackClicks(document.body);
+  document.body.querySelectorAll('form').forEach(trackFormSubmits);
+  document.body.querySelectorAll('input').forEach(trackInputChanges);
+  document.body.querySelectorAll('select').forEach(trackSelectInputs);
+}
 
-      default:
-        break;
+function initIframe(iframe) {
+  if (iframes.indexOf(iframe) > -1) {
+    return;
+  }
+
+  iframes.push(iframe);
+  addMutationObserver(iframe.contentDocument.body);
+
+  iframe.addEventListener('mouseenter', function (e) {
+    stack.unshift(this);
+    iframe.contentDocument.querySelectorAll('iframe').forEach(initIframe);
+    sendToHost({
+      className: 'SwitchTo',
+      locators: generateLocators(stack[0]),
+      boundingBox: getBoundingClientRect()
+    });
+  });
+
+  iframe.addEventListener('mouseleave', function (e) {
+    if (stack.length > 0) {
+      stack.shift();
+      sendToHost({
+        className: 'SwitchToParent',
+        locators: generateLocators(stack[0]),
+        boundingBox: getBoundingClientRect()
+      });
+    } else {
+      sendToHost({
+        type: 'SwitchToDefaultContent'
+      });
     }
   });
+
+  waitUntilIframeIsLoaded(iframe, () => {
+    const doc = iframe.contentDocument;
+    trackMouseMovements(doc);
+    trackClicks(doc);
+    doc.querySelectorAll('form').forEach(trackFormSubmits);
+    doc.querySelectorAll('input').forEach(trackInputChanges);
+    doc.querySelectorAll('select').forEach(trackSelectInputs);
+  });
+}
+
+function trackMouseMovements(el) {
+  el.addEventListener('mousemove', function (e) {
+    if (currentTarget !== e.target) {
+      currentTarget = e.target;
+    }
+  });
+}
+
+function trackClicks(el) {
+  el.addEventListener('click', function (e) {
+    sendToHost({
+      className: 'Click',
+      locators: generateLocators(this),
+      boundingBox: currentTarget.getBoundingClientRect()
+    });
+  });
+}
+
+function trackFormSubmits(el) {
+  el.addEventListener('submit', function (e) {
+    sendToHost({
+      className: 'Submit',
+      locators: generateLocators(this),
+      boundingBox: currentTarget.getBoundingClientRect()
+    });
+  });
+}
+
+function trackInputChanges(el) {
+  let input = '';
+
+  el.addEventListener('input', function (e) {
+    input = this.value;
+  });
+
+  el.addEventListener('blur', function (e) {
+    if (input === '') {
+      sendToHost({
+        className: 'Clear',
+        locators: generateLocators(this),
+        boundingBox: this.getBoundingClientRect()
+      });
+    } else {
+      sendToHost({
+        className: 'Type',
+        locators: generateLocators(this),
+        boundingBox: this.getBoundingClientRect(),
+        value: this.value
+      });
+    }
+  });
+}
+
+function trackSelectInputs(el) {
+  el.addEventListener('change', function (e) {
+    sendToHost({
+      className: 'Select',
+      locators: generateLocators(this),
+      boundingBox: this.getBoundingClientRect(),
+      value: this.value
+    });
+  });
+}
+
+function waitUntilIframeIsLoaded(iframe, fn) {
+  const win = iframe.contentWindow;
+  if (win && win.document && win.document.body && win.document.body.innerHTML) {
+    fn();
+    return;
+  }
+
+  window.setTimeout(() => {
+    waitUntilIframeIsLoaded(iframe, fn);
+  }, 100);
+}
+
+function addMutationObserver(target) {
+  const mo = new MutationObserver((records, instance) => {
+    console.log(records);
+    records.forEach((record) => {
+      record.addedNodes.forEach((node) => {
+
+        sendToHost({
+          className: 'WaitForAddedNode',
+          locators: generateLocators(node),
+          boundingBox: node.getBoundingClientRect(),
+          maxWaitTime: 3000
+        });
+
+        switch (node.nodeName) {
+          case 'IFRAME':
+            initIframe(node);
+            break;
+          case 'FORM':
+            trackFormSubmits(node);
+            break;
+          case 'INPUT':
+            trackInputChanges(node);
+            break;
+          case 'SELECT':
+            trackSelectInputs(node);
+            break;
+          default:
+            if (node.nodeName !== '#text') {
+              node.querySelectorAll('iframe').forEach(initIframe);
+              node.querySelectorAll('form').forEach(trackFormSubmits);
+              node.querySelectorAll('input').forEach(trackInputChanges);
+              node.querySelectorAll('select').forEach(trackSelectInputs);
+            }
+        }
+      });
+
+      record.removedNodes.forEach((node) => {
+        sendToHost({
+          className: 'WaitForRemovedNode',
+          locators: generateLocators(node),
+          boundingBox: node.getBoundingClientRect(),
+          maxWaitTime: 3000
+        });
+      });
+
+      if (record.attributeName != null) {
+        const node = record.target;
+        sendToHost({
+          className: 'WaitForNodeAttribute',
+          locators: generateLocators(node),
+          boundingBox: node.getBoundingClientRect(),
+          attribute: record.attributeName,
+          value: node.getAttribute(record.attributeName),
+          maxWaitTime: 3000
+        });
+      }
+    });
+  });
+  const options = {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    characterData: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+  };
+  mo.observe(target, options);
+}
+
+function generateBasicCssSelector(el) {
+  const names = [];
+  while (el.parentNode) {
+    if (el.id) {
+      names.unshift('#' + el.id);
+      break;
+    } else {
+      if (el === el.ownerDocument.documentElement) {
+        names.unshift(el.tagName);
+      } else {
+        for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+        names.unshift(el.tagName + ':nth-child(' + c + ')');
+      }
+      el = el.parentNode;
+    }
+  }
+  return names.join(' > ');
+}
+
+function generateLocators(el) {
+  let locators = [];
+
+  locators.push({ methodName: 'Basic', className: 'CssLocator', value: generateBasicCssSelector(el) });
+  locators.push({ methodName: 'CssSelectorGenerator', className: 'CssLocator', value: cssSelectorGenerator.getSelector(el) });
+  locators.push({ methodName: 'Finder', className: 'CssLocator', value: myapi.finder(el) });
+  locators.push({ methodName: 'GetQuerySelector', className: 'CssLocator', value: myapi.getQuerySelector(el) });
+  locators.push({ methodName: 'OptimalSelect', className: 'CssLocator', value: myapi.select(el) });
+  locators.push({ methodName: 'SelectorQuery', className: 'CssLocator', value: myapi.selectorQuery(el) });
+  locators.push({ methodName: 'RobulaPlus', className: 'XpathLocator', value: robulaPlus.getRobustXPath(el, document) });
+
+  return locators;
+}
+
+function sendToHost(message) {
+  console.log('Sending ' + message.className + ' Action...');
+  ipcRenderer.sendToHost(JSON.stringify(message));
 }
